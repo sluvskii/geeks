f2290aa09298186a3ea6598f7b9e1ab4479a7aa9
sluvskii <okroshka.p@gmail.com>
2026-05-06 12:08:47 +0500

    Added basic structure and CI for IPA build

---
+++ MusicGeeks/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+import AVFoundation
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootVC = ViewController()
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
